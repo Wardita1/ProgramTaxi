87e27f70b2385b219a04044cb0d9bb348f802dac
ASUS <ASUS@DESKTOP-9EG5LU8>
2023-03-21 14:14:06 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Latihan;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DrivName = "Wardita";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan
+{
+    public class Taxi
+    {
+        public string DrivName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DrivName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("OnDuty: Yes");
+            }
+            else if (OnDuty = false)
+            {
+                Console.WriteLine("OnDuty: No");
+            }
+            Console.WriteLine("Number Of Passenger: {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\nWardita sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Waradita" +
+                "" +
+                " selesai mengantar penumpang");
+        }
+    }
+}
